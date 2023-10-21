fdffb88dea80da6cf62cad485df649a0316600bb
Creatimatix <creatimatix@gmail.com>
2023-10-21 19:58:49 +0530

    api related issue

--- app/Models/Admin/Product.php
+++ app/Models/Admin/Product.php
@@ -10,13 +10,21 @@ class Product extends BaseModel
 {
     use HasFactory;
 
-    protected $appends = ['default_image'];
+    protected $appends = ['default_image','default_document'];
 
     public function getDefaultImageAttribute(){
         return $this->image;
     }
+
+    public function getDefaultDocumentAttribute(){
+        return $this->documents;
+    }
     public function image($isMultiple = true){
-        return $this->hasOne(ProductImage::class, 'id_product','id');
+        return $this->hasOne(ProductImage::class, 'id_product','id')->where('type', 0)->orderBy('id', 'desc');
+    }
+
+    public function documents($isMultiple = true){
+        return $this->hasOne(ProductImage::class, 'id_product','id')->where('type', 1)->orderBy('id', 'desc');
     }
 
     public function category(){
@@ -26,4 +34,6 @@ class Product extends BaseModel
     public function images(){
         return $this->hasMany(ProductImage::class, 'id_product', 'id');
     }
+
+
 }
